f8dc624b6d7a6ed974c1d2bee927744eec240ed8
jlangree <biglangree@gmail.com>
2019-10-12 10:17:35 -0500

    stuff 1017 am

---
+++ schema_lake_levels.py
@@ -0,0 +1,22 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, Boolean, Date, Float
+from sqlalchemy import Table, PrimaryKeyConstraint
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Lake_levels(Base):
+    __tablename__ = 'lake_levels'
+    id = Column(Integer())
+    elevation = Column(Float())
+    read_date = Column(Date())
+    datum_adj = Column(String())
+    __table_args__ = (
+        PrimaryKeyConstraint('id', 'read_date'),
+        {},
+    )
+
+class Lake_names(Base):
+    __tablename__ = 'lake_names'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
